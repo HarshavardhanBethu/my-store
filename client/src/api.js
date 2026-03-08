65d9d7d2621b4598602f57f9d3295bd848c5bfda
HarshavardhanBethu <harshavardhanbethu2205@gmail.com>
2026-03-08 07:41:45 +0530

    Set default API_URL if not provided

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://harsha-store.onrender.com";
 
 export const createOrder = async (orderData) => {
   const res = await fetch(`${API_URL}/api/orders`, {
